c0037bde909ad2e45f31c8e440fa45d55c1e2678
Alexander <alexander.y.peng@gmail.com>
2019-08-25 18:17:35 -0700

    Refactors codebase, splits into playlist and controls

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import AersiaServices from './services/AersiaServices';
 import SongTable from './components/SongTable';
-import ControlButton from './components/ControlButton';
-import ProgressBar from './components/ProgressBar';
+import PlayerControls from './components/PlayerControls';
 
 const parser = require('fast-xml-parser');
 
 function App() {
-  const player = useRef();
-  const trackBar = useRef();
-  const point = useRef();
-  const dragging = useRef();
-
   const [songs, setSongs] = useState([]);
-  const [currentTrackMoment, setCurrentTrackMoment] = useState(0);
-  const [currentTrackDuration, setCurrentTrackDuration] = useState('00:00');
-  const [playlist, setPlaylist] = useState('VIP');
+  const [playlist, setPlaylist] = useState('mellow');
   const [selectedSong, setSelectedSong] = useState(null);
-  const [progressBarWidth, setProgressBarWidth] = useState(0);
-
-  const [play, setPlay] = useState(false);
-  const [shuffle, setShuffle] = useState(true);
-  const [previousSongs, setPreviousSongs] = useState([]);
 
   // on initial load, grabs playlists
   useEffect((() => {
@@ -30,9 +17,8 @@ function App() {
       .then(playlist => {
         if (!playlist || parser.validate(playlist)) {
           const jsonObj = parser.parse(playlist);
-          console.log(jsonObj, jsonObj.playlist.trackList.track);
-          setSongs(cleanTracklist(jsonObj.playlist.trackList.track));
-          // setSongs(jsonObj.playlist.trackList.track);
+          const cleanSongs = cleanTracklist(jsonObj.playlist.trackList.track);
+          setSongs(cleanSongs);
         } else {
           console.log('error, invalid playlist xml format', playlist);;
           throw new Error('invalid playlist');
@@ -50,14 +36,12 @@ function App() {
       if (!response || parser.validate(response)) {
         const jsonObj = parser.parse(response);
         setSongs(cleanTracklist(jsonObj.playlist.trackList.track));
-        // setSongs(jsonObj.playlist.trackList.track);
       } else {
         throw new Error('invalid playlist xml format');
       }
     } catch (exception) {
         console.log(exception);
     }
-
   }
 
   // ensures uniqueness, removes additional info from 
@@ -75,191 +59,9 @@ function App() {
     songMap = null;
     return trackArrays;
   }
-
-  function randomSongPosition() {
-    return Math.floor(Math.random() * songs.length);
-  }
-
+  
   function selectSong(song) {
-    if (selectedSong === null) {
-      setSelectedSong(song);
-    } else {
-      setPreviousSongs(previousSongs.concat(selectedSong));
-      setSelectedSong(song);
-    }
-    console.log('select song: ', song);
-    player.current.src = song.location;
-    player.current.play();
-    setPlay(true);
-  }
-
-  function handlePlayPause() {
-    if (selectedSong === null) { // init load, no song selected yet
-      const songPosition = shuffle ? randomSongPosition() : 0;
-      const randomSong = songs[songPosition];
-      selectSong(randomSong);
-    } else if (play === true) { // song currently playing, pause
-      player.current.pause();
-      setPlay(false);
-    } else { // song currently paused, play
-      player.current.play();
-      setPlay(true);
-    }
-  }
-
-  function handleTimeUpdate() {
-    setCurrentTrackMoment(Math.floor(player.current.currentTime));
-    if (!dragging.current) {
-      // console.log(player.current.currentTime);
-      // console.log('updating width', player.current.currentTime / player.current.duration * 100)
-      // setProgressBarWidth(
-      //   (Math.floor(player.current.currentTime) / player.current.duration) * 100 + '%'
-      // );
-      setProgressBarWidth(
-        ((player.current.currentTime / player.current.duration) * 100) + '%'
-        );
-    }
-  }
-
-  function handleLoadedMetaData() {
-    setCurrentTrackDuration(Math.floor(player.current.duration));
-  }
-
-  function getPreviousSong() {
-    if (previousSongs.length > 0) {
-      const song = previousSongs[previousSongs.length - 1];
-      setPreviousSongs(previousSongs.slice(0, previousSongs.length - 1));
-      player.current.src = song.location;
-      player.current.play();
-    }
-  }
-
-  function getNextSong() {
-    const songPosition = shuffle ? randomSongPosition() :
-      songs.findIndex(song => song === selectedSong) + 1;
-    const nextSong = songs[songPosition];
-    selectSong(nextSong);
-  }
-
-  function handleShuffle() {
-    setShuffle(!shuffle);
-  }
-
-  function handleVolume() {
-
-  }
-
-  function handleSecondsToMinutes(sec) {
-    if (sec === 0 || isNaN(sec)) return '00:00'
-    let minutes = Math.floor(sec / 60) + '';
-    let seconds = sec - minutes * 60 + ''; 
-    minutes = minutes.length === 1 ? '0' + minutes : minutes;
-    seconds = seconds.length === 1 ? '0' + seconds : seconds;
-    return `${minutes}:${seconds}`;
-  }
-
-  function handlePointPosition(position) {
-    // let trackBarWidth = trackBar.current.offsetWidth - point.current.offsetWidth;
-    let trackBarWidth = trackBar.current.offsetWidth;
-    let handleLeft = position - trackBar.current.offsetLeft;
-    let pointPercent = ((handleLeft / trackBarWidth) * 100);
-
-    if (handleLeft > 0 && handleLeft < trackBarWidth) {
-      setProgressBarWidth(pointPercent + '%');
-    } else if (handleLeft < 0) {
-      setProgressBarWidth('0%');
-    } else if (handleLeft > trackBarWidth) {
-      setProgressBarWidth('100%');
-    }
-    
-    // console.log('handle point position', pointPercent);
-    // console.log(handleLeft, trackBarWidth, point.current.offsetWidth, (handleLeft / trackBarWidth) * 100);
-
-  }
-
-  function handleMouseMove(e) {
-    dragging.current = true;
-    handlePointPosition(e.pageX);
-  }
-
-  function handleMouseUp(e) {
-    // dragging.current = false;
-    // let trackBarWidth = trackBar.current.offsetWidth - point.current.offsetWidth;
-    let trackBarWidth = trackBar.current.offsetWidth;
-    let handleLeft = e.pageX - trackBar.current.offsetLeft;
-    let pointPercent = (handleLeft / trackBarWidth)
-    // console.log('mouse up', pointPercent)
-
-    if (selectedSong) {
-      player.current.currentTime = (pointPercent * player.current.duration);
-      // player.current.currentTime = Math.floor(((e.pageX - trackBar.current.offsetLeft) / (trackBar.current.offsetWidth - point.current.offsetWidth)) * player.current.duration);
-    }
-    dragging.current = false;
-    window.removeEventListener('mousemove', handleMouseMove);
-    window.removeEventListener('mouseup', handleMouseUp);
-  }
-
-  function handleMouseDown(e) {
-    dragging.current = true;
-    handlePointPosition(e.pageX);
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);
-  }
-
-  function controlPlayer() {
-    const playerStyle = {
-      position: 'fixed',
-      paddingBottom: '10px',
-      bottom: '0',
-      width: '100%',
-      backgroundColor: 'white'
-    }
-    
-    const controlStyle = {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-
-    const progressStyle = {
-      display: 'flex',
-      alignItems: 'center',
-    }
-
-    const timeStyle = {
-      margin: '0 1rem'
-    }
-
-    return (
-      <div style={playerStyle}>
-        <div style={progressStyle}>
-          <p style={timeStyle}>{handleSecondsToMinutes(currentTrackMoment)}</p>
-          <ProgressBar 
-            progressPercent={progressBarWidth} 
-            trackBarRef={trackBar} 
-            pointRef={point} 
-            mouseDown={handleMouseDown}
-          />
-          <p style={timeStyle}>{handleSecondsToMinutes(currentTrackDuration - currentTrackMoment)}</p>
-        </div>
-        <div style={controlStyle}>
-          {shuffle ? 
-            <ControlButton msg='linear' click={handleShuffle} /> :
-            <ControlButton msg='shuffle' click={handleShuffle} />
-          }
-          <ControlButton msg='prev' click={getPreviousSong} />
-          {play ? 
-            <ControlButton msg='pause' click={handlePlayPause} /> :
-            <ControlButton msg='play' click={handlePlayPause} /> 
-          }
-          <ControlButton msg='next' click={getNextSong}/>
-          <ControlButton msg='volume' click={handleVolume} />
-          <select value={playlist} onChange={(event) => getPlaylist(event)}>
-            {PLAYLIST_OPTIONS.map(playlist => <option value={playlist} key={playlist}>{playlist}</option>)}
-          </select>
-        </div>
-      </div>
-    )
+    setSelectedSong(song);
   }
 
   if (songs.length === 0) {
@@ -272,15 +74,11 @@ function App() {
 
   return (
     <div>
-      
+      <select value={playlist} onChange={(event) => getPlaylist(event)}>
+        {PLAYLIST_OPTIONS.map(playlist => <option value={playlist} key={playlist}>{playlist}</option>)}
+      </select>
       <SongTable songs={songs} selectSong={selectSong} selectedSong={selectedSong}/>
-      <audio 
-      ref={player}
-      onTimeUpdate={() => handleTimeUpdate()}
-      onLoadedMetadata={() => handleLoadedMetaData()}
-      onEnded={() => getNextSong()} 
-      />
-      {controlPlayer()}
+      <PlayerControls songs={songs} selectSong={selectSong} selectedSong={selectedSong} />
     </div>
   );
 }
---
+++ src/components/PlayerControls.js
@@ -0,0 +1,211 @@
+import React, {useState, useEffect, useRef} from 'react';
+import ControlButton from './ControlButton';
+import ProgressBar from './ProgressBar';
+
+
+function PlayerControls({songs, selectSong, selectedSong}) {
+
+  const player = useRef();
+  const trackBar = useRef();
+  const point = useRef();
+  const dragging = useRef();
+
+  const [currentTrackMoment, setCurrentTrackMoment] = useState(0);
+  const [currentTrackDuration, setCurrentTrackDuration] = useState(0);
+  const [progressBarWidth, setProgressBarWidth ] = useState(0);
+
+  const [play, setPlay] = useState(false);
+  const [shuffle, setShuffle] = useState(true);
+  const [previousSongs, setPreviousSongs] = useState([]);
+
+  useEffect(() => {
+    setCurrentTrackMoment(0);
+    setCurrentTrackDuration(0);
+    setProgressBarWidth(0);
+    handleNewSong();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[selectedSong])
+
+  function randomSongPosition() {
+    return Math.floor(Math.random() * songs.length - 1);
+  }
+
+  function handleNewSong() {
+    if (selectedSong) {
+      setPreviousSongs(previousSongs.concat(selectedSong));
+      player.current.src = selectedSong.location;
+      player.current.play();
+      setPlay(true);
+    }
+  }
+
+  function handlePlay() {
+    if (selectedSong === null) {
+      const songPosition = shuffle ? randomSongPosition() : 0;
+      const randomSong = songs[songPosition];
+      selectSong(randomSong);
+    } else if (play === true) {
+      player.current.pause();
+      setPlay(false);
+    } else {
+      player.current.play()
+      setPlay(true);
+    }
+  }
+
+  function handleTimeUpdate() {
+    setCurrentTrackMoment(Math.floor(player.current.currentTime));
+    if (!dragging.current) {
+      setProgressBarWidth(
+        ((player.current.currentTime / player.current.duration) * 100) + '%'
+      );
+    }
+  }
+
+  function handleLoadedMetaData() {
+    setCurrentTrackDuration(Math.floor(player.current.duration));
+  }
+
+  function handlePreviousSong() {
+    if (previousSongs.length > 1) {
+      const song = previousSongs[previousSongs.length - 2];
+      setPreviousSongs(previousSongs.slice(0, previousSongs.length - 2));
+      selectSong(song);
+    }
+  }
+
+  function handleNextSong() {
+    const songPosition = shuffle ? 
+      randomSongPosition() :
+      songs.findIndex(song => song === selectedSong) + 1;
+    const nextSong = songs[songPosition];
+    selectSong(nextSong);
+  }
+
+  function handleShuffle() {
+    setShuffle(!shuffle);
+  }
+
+  function handleVolume() {
+    
+  }
+
+  function handleSecondsToMinutes(sec) {
+    if (sec === 0 || isNaN(sec)) return '00:00'
+    let minutes = Math.floor(sec / 60) + '';
+    let seconds = sec - minutes * 60 + ''; 
+    minutes = minutes.length === 1 ? '0' + minutes : minutes;
+    seconds = seconds.length === 1 ? '0' + seconds : seconds;
+    return `${minutes}:${seconds}`;
+  }
+
+  function handlePointPosition(position) {
+    const trackBarWidth = trackBar.current.offsetWidth;
+    const handleLeft = position - trackBar.current.offsetLeft;
+    const pointPercent = ((handleLeft / trackBarWidth) * 100);
+
+    if (handleLeft > 0 && handleLeft < trackBarWidth) {
+      setProgressBarWidth(pointPercent + '%');
+    } else if (handleLeft < 0) {
+      setProgressBarWidth('0%');
+    } else if (handleLeft > trackBarWidth) {
+      setProgressBarWidth('100%');
+    }
+  }
+
+  function handleMouseMove(e) {
+    dragging.current = true;
+    handlePointPosition(e.pageX);
+  }
+
+  function handleMouseUp(e) {
+    const trackBarWidth = trackBar.current.offsetWidth;
+    const handleLeft = e.pageX - trackBar.current.offsetLeft;
+    const pointPercent = (handleLeft / trackBarWidth);
+
+    if (selectedSong) {
+      player.current.currentTime = (pointPercent * player.current.duration);
+    }
+    dragging.current = false;
+    window.removeEventListener('mousemove', handleMouseMove);
+    window.removeEventListener('mouseup', handleMouseUp);
+  }
+
+  function handleMouseDown(e) {
+    dragging.current = true;
+    handlePointPosition(e.pageX);
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+  }
+
+  function controlPlayer() {
+    const playerStyle = {
+      position: 'fixed',
+      paddingBottom: '10px',
+      bottom: '0',
+      width: '100%',
+      backgroundColor: 'white'
+    }
+    
+    const controlStyle = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+
+    const progressStyle = {
+      display: 'flex',
+      alignItems: 'center',
+    }
+
+    const timeStyle = {
+      margin: '0 1rem'
+    }
+
+    return (
+      <div style={playerStyle}>
+        <div style={progressStyle}>
+          <p style={timeStyle}>{handleSecondsToMinutes(currentTrackMoment)}</p>
+          <ProgressBar 
+            progressPercent={progressBarWidth} 
+            trackBarRef={trackBar} 
+            pointRef={point} 
+            mouseDown={handleMouseDown}
+          />
+          <p style={timeStyle}>{handleSecondsToMinutes(currentTrackDuration - currentTrackMoment)}</p>
+        </div>
+        <div style={controlStyle}>
+          {shuffle ? 
+            <ControlButton msg='linear' click={handleShuffle} /> :
+            <ControlButton msg='shuffle' click={handleShuffle} />
+          }
+          <ControlButton msg='prev' click={handlePreviousSong} />
+          {play ? 
+            <ControlButton msg='pause' click={handlePlay} /> :
+            <ControlButton msg='play' click={handlePlay} /> 
+          }
+          <ControlButton msg='next' click={handleNextSong}/>
+          <ControlButton msg='volume' click={handleVolume} />
+          
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <audio 
+      ref={player}
+      onTimeUpdate={() => handleTimeUpdate()}
+      onLoadedMetadata={() => handleLoadedMetaData()}
+      onEnded={() => handleNextSong()} 
+      >
+        Oops
+      </audio>
+      {controlPlayer()}
+    </div>
+  )
+}
+
+export default PlayerControls;
