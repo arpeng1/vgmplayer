682c7e31e21bb705beabf72e170d36479a9b3fb4
Alexander <alexander.y.peng@gmail.com>
2019-08-27 22:19:22 -0700

    Updates

---
+++ precache-manifest.ab40e7dbf846ef9ffceedf310a90b1b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cf5b71f7227ee8c8639d2bae8726bf",
+    "url": "/vgmplayer/index.html"
+  },
+  {
+    "revision": "8af0ee4137bf3d0a6658",
+    "url": "/vgmplayer/static/css/main.4568f3a2.chunk.css"
+  },
+  {
+    "revision": "6a6afe32b20ce7acdce5",
+    "url": "/vgmplayer/static/js/2.0ff2b1ea.chunk.js"
+  },
+  {
+    "revision": "8af0ee4137bf3d0a6658",
+    "url": "/vgmplayer/static/js/main.1d5ce772.chunk.js"
+  },
+  {
+    "revision": "6a1b30339fd453b96855",
+    "url": "/vgmplayer/static/js/runtime~main.89abf831.js"
+  }
+]);
